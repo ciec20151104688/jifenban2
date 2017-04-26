93d90d3202dffaba2ac7082eaba7516abb205bd6
ciec20151104688 <s20151104688@mac05.local>
2017-04-26 11:24:10 +0800

    jifenban2
    123

--- jifenban2/ViewController.swift
+++ jifenban2/ViewController.swift
@@ -9,17 +9,51 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var showone: UILabel!
+    @IBOutlet weak var showtwo: UILabel!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func lefttwo(sender: AnyObject) {
+        var score:Int;
+        score = Int(showone.text!)!;
+        score = score + 1;
+        showone.text = ("\(score)");
     }
 
+        @IBAction func leftone(sender: AnyObject) {
+            var score:Int;
+            score = Int(showone.text!)!;
+            if score>0{
+                score = score - 1;
+                showone.text = ("\(score)");
+        }
+            }
+
+    @IBAction func rightone(sender: AnyObject) {
+        var score:Int;
+        score = Int(showtwo.text!)!;
+        if score>0{
+            score = score - 1;
+            showtwo.text = ("\(score)");
+        
+    }
+        }
+    @IBAction func righttwo(sender: AnyObject) {
+        var score:Int;
+        score = Int(showtwo.text!)!;
+        score = score + 1;
+        showtwo.text = ("\(score)");
+    }
+        override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
