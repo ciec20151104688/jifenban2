8c43a18da3562ff6aab122e711ede54a95cea215
ciec20151104688 <s20151104688@mac05.local>
2017-04-26 10:43:07 +0800

    Initial Commit

---
+++ jifenban2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jifenban2
+//
+//  Created by liuyuxuan on 17/4/26.
+//  Copyright © 2017年 liuyuxuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ jifenban2Tests/jifenban2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  jifenban2Tests.swift
+//  jifenban2Tests
+//
+//  Created by liuyuxuan on 17/4/26.
+//  Copyright © 2017年 liuyuxuan. All rights reserved.
+//
+
+import XCTest
+@testable import jifenban2
+
+class jifenban2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
